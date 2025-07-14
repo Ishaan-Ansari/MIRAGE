0369af92b8ee06d670d760febccc6beb69d62ed8
ishaan-ansari <ishaanansari044@gmail.com>
2025-07-14 16:06:16 +0530

    Implemented embedding class

---
+++ embedder.py
@@ -0,0 +1,7 @@
+from typing import List
+
+from utilities.embeddings import get_openai_embeddings
+
+class PipelineEmbeddings:
+    def get_embeddings(self, texts: List[str]) -> List[List[float]]:
+        return get_openai_embeddings(texts)
