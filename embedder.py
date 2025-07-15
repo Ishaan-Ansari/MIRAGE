80bc780c1d55837d1e90e4ac926244f767aeb5b5
ishaan-ansari <ishaanansari044@gmail.com>
2025-07-15 12:32:21 +0530

    Implemented function for async embeddings gen

--- embedder.py
+++ embedder.py
@@ -4,4 +4,4 @@ from utilities.embeddings import get_openai_embeddings
 
 class PipelineEmbeddings:
     def get_embeddings(self, texts: List[str]) -> List[List[float]]:
-        return get_openai_embeddings(texts)
+        return get_openai_embeddings(texts, batch_size=10)
--- utilities/embeddings.py
+++ utilities/embeddings.py
@@ -6,7 +6,7 @@ from config import OPENAI_API_KEY
 from constants import DEFAULT_OPENAI_EMBEDDING_MODEL
 
 OPENAI_CLIENT = OpenAI(api_key=OPENAI_API_KEY)
-ASYNC_OPENAI_CLIENT = AsyncOpenAI(api_key=OPENAI_API_KEY)
+OPENAI_ASYNC_CLIENT = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
 def get_openai_embeddings(texts: List[str], batch_size: int) -> List[List[float]]:
     if not isinstance(texts, list):
@@ -24,4 +24,16 @@ def get_openai_embeddings(texts: List[str], batch_size: int) -> List[List[float]
     return embeddings
 
 async def get_openai_embeddings_async(texts: List[str], batch_size: int) -> List[List[float]]:
-    pass
+    if not isinstance(texts, list):
+        raise ValueError("texts must be a list")
+
+    batch_size = [texts[i : i + batch_size] for i in range(0, len(texts), batch_size)]
+    embeddings = []
+    for batch in batch_size:
+        response = await OPENAI_ASYNC_CLIENT.embeddings.create(
+            input=batch,
+            model=DEFAULT_OPENAI_EMBEDDING_MODEL
+        )
+        _embeddings = [response.data[0].embedding for _ in range(len(batch))]
+        embeddings.extend(_embeddings)
+    return embeddings
